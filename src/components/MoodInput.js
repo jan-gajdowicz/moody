4aca69ca989291a57134c887d35097c6b09bec67
Jan Gajdowicz <jan.gajdowicz@gmail.com>
2020-02-09 23:59:42 +0100

    Add wizzard to form

--- src/components/MoodInput.js
+++ src/components/MoodInput.js
@@ -4,18 +4,30 @@ import 'react-rangeslider/lib/index.css'
 
 import { SCALE } from '../config/index.js'
 
-const MoodInput = ({ emotion }) => {
+const MoodInput = ({ index, emotion, activeInput, setActiveInput }) => {
   const [value, setValue] = useState(0)
+  const active = activeInput === index ? '--active' : ''
   const handleSliderChange = value => {
     setValue(value)
   }
+  const handleSliderComplete = () => {
+    setActiveInput(index + 1)
+  }
+
   const min = SCALE[0]
   const max = SCALE.length - 1
 
   return (
-    <div className="mood-input__container">
+    <div className={`mood-input__container${active}`}>
       <h2 className="mood-input__header">{emotion}</h2>
-      <Slider labels={SCALE} max={max} min={min} onChange={handleSliderChange} value={value} />
+      <Slider
+        labels={SCALE}
+        max={max}
+        min={min}
+        onChange={handleSliderChange}
+        onChangeComplete={handleSliderComplete}
+        value={value}
+      />
     </div>
   )
 }
--- src/components/MoodLoggerForm.js
+++ src/components/MoodLoggerForm.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import MoodInput from '../components/MoodInput'
 import { EMOTIONS } from '../config'
 
 const MoodLoggerForm = () => {
+  const [activeInput, setActiveInput] = useState(0)
+  const handleInputChange = value => {
+    setActiveInput(value)
+  }
+
   return (
     <div className="mood-logger-form__container">
-      {EMOTIONS.map((emotion, index) => <MoodInput emotion={emotion} key={index} />)}
+      {EMOTIONS.map((emotion, index) => (
+        <MoodInput
+          activeInput={activeInput}
+          emotion={emotion}
+          index={index}
+          key={index}
+          setActiveInput={handleInputChange}
+        />
+      ))}
     </div>
   )
 }
