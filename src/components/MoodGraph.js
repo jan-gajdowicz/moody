a76ed9035e61f95d17dad4e37af16814486f4a96
Jan Gajdowicz <jan.gajdowicz@gmail.com>
2020-02-11 21:54:15 +0100

    Fix filters

--- src/components/MoodGraph.js
+++ src/components/MoodGraph.js
@@ -3,28 +3,46 @@ import { CartesianGrid, XAxis, Line, YAxis, Tooltip, LineChart } from 'recharts'
 import { EMOTIONS } from '../config'
 
 const MoodGraph = ({ moods }) => {
-  const [filteredEmotions, setFilteredEmotions] = useState([])
+  const [filters, setFilters] = useState(EMOTIONS)
 
-  const filterEmotions = name => () => {
-    const filtered = EMOTIONS.filter(emotion => emotion.name === name)
-    setFilteredEmotions(filtered)
+  const isInFilters = emotion => filters.includes(emotion)
+
+  const filterEmotions = emotion => () => {
+    if (isInFilters(emotion)) {
+      return setFilters(filters.filter(filter => filter !== emotion))
+    }
+    setFilters([...filters, emotion])
   }
   return (
     <div className="mood-graph__container">
       <div className="mood-graph__filters">
-        {EMOTIONS.map(({ name, color }, index) => (
-          <div className="mood-graph__filter" key={index} style={{ border: `1px solid ${color}` }}>
-            <input checked id={name} name={name} onChange={filterEmotions(name)} type="checkbox" />
-            <label htmlFor={name}>{name}</label>
-          </div>
-        ))}
+        {JSON.stringify(filters)}
+        {EMOTIONS.map((emotion, index) => {
+          const { name, color } = emotion
+          return (
+            <div
+              className="mood-graph__filter"
+              key={index}
+              style={{ border: `1px solid ${color}` }}
+            >
+              <input
+                checked={isInFilters(emotion)}
+                id={name}
+                name={name}
+                onChange={filterEmotions(emotion)}
+                type="checkbox"
+              />
+              <label htmlFor={name}>{name}</label>
+            </div>
+          )
+        })}
       </div>
       <LineChart data={moods} height={400} width={800}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="emotions.date" />
         <YAxis />
         <Tooltip />
-        {filteredEmotions.map(({ name, color }, index) => (
+        {filters.map(({ name, color }, index) => (
           <Line
             dataKey={`emotions.${name}`}
             key={index}
