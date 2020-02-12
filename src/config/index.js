590dfe6ef67348c9d3ba0b35b95e7580ca611686
Jan Gajdowicz <jan.gajdowicz@gmail.com>
2020-02-13 00:50:28 +0100

    Add scopes

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,12 @@ export const EMOTIONS = [
   { name: 'happiness', color: '#FFBB28' },
   { name: 'focus', color: '#FF8042' },
 ]
+export const SCOPES = [
+  { name: 'week', value: 7 },
+  { name: 'month', value: 30 },
+  { name: 'year', value: 365 },
+  { name: 'all-time', value: false },
+]
+export const DEFAULT_SCOPE = 7
 export const SCALE = [0, 1, 2, 3, 4, 5]
 export const TEST_DELAY = 500
