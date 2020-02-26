a6e8650eaa2309d25450d7028016941396fc037f
Jan Gajdowicz <jan.gajdowicz@gmail.com>
2020-02-26 16:32:43 +0100

    Fix Onboarder flow

--- src/components/Nav/NavHeader.js
+++ src/components/Nav/NavHeader.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import ScreenModeSwitcher from './ScreenModeSwitcher'
 
 import { AppContext } from 'contexts/AppContext'
@@ -11,7 +12,11 @@ const NavHeader = () => {
   return (
     <div className="nav-header__container">
       <div className="nav-header">
-        <div className="nav-header__logo">moody</div>
+        <div className="nav-header__logo">
+          <Link className="nav-header__link" to="/">
+            moody
+          </Link>
+        </div>
         <div className="nav-header__tools">
           <ScreenModeSwitcher classProp="nav-header__button" />
           <div className="nav-header__button" onClick={handleButton}>
--- src/components/Onboarder.js
+++ src/components/Onboarder.js
@@ -1,25 +1,23 @@
 import React, { useContext } from 'react'
 
-import { AppContext } from 'contexts/AppContext'
 import { WizzardProvider } from 'contexts/WizzardContext'
+import { AppContext } from 'contexts/AppContext'
 import Wizzard from 'components/Wizzard'
 import WizzardFieldSwitcher from 'components/Wizzard/WizzardFieldSwitcher'
 import { ONBOARD_STEPS } from 'config'
 
 export default function Onboarder() {
-  const { showWizzard } = useContext(AppContext)
-  const storeUserConfig = config => console.log(config)
+  const { toggleWelcomePage, handleWizzard } = useContext(AppContext)
+  const storeUserConfig = config => {
+    console.log(config)
+    toggleWelcomePage(false)
+    handleWizzard(false)
+  }
   return (
     <div className="onboarder__container">
       <div className="onboarder">
         <WizzardProvider>
-          {showWizzard && (
-            <Wizzard
-              child={WizzardFieldSwitcher}
-              mutation={storeUserConfig}
-              steps={ONBOARD_STEPS}
-            />
-          )}
+          <Wizzard child={WizzardFieldSwitcher} mutation={storeUserConfig} steps={ONBOARD_STEPS} />
         </WizzardProvider>
       </div>
     </div>
